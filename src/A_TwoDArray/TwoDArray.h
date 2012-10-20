e803bb80b96caa39365a17e2031ae0d3ddfcf55e
Travace Reese <tsuk13@gmail.com>
2012-10-20 14:58:29 -0400

    set up A_TwoDArray header

--- src/A_TwoDArray/TwoDArray.h
+++ src/A_TwoDArray/TwoDArray.h
@@ -1,1 +1,29 @@
+#ifndef __TWODARRAY__
+#define __TWODARRAY__
 
+template <typename T>
+class TwoDArray{
+  private:
+    T** array;
+    int rows;
+    int cols;
+  
+  public:
+    //constructor
+    TwoDArray<T>(int r, int c, T value);
+    //deconstructor
+    ~TwoDArray<T>();
+    //replaces value at row r and col c
+    void insert(int r, int c, T value);
+    //returns value at row r and col c
+    T access(int r, int c);
+    //sets value at row r and col c to default value
+    void remove(int r, int c);
+    //prints array
+    void print();
+    //getters and setters for iteration
+    int getNumRows();
+    int getNumCols();
+};
+
+#endif
